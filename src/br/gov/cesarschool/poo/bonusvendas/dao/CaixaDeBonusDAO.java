abd31357ea4be83683d8315af3b7f888526617a3
Gabriel Pires <gabrielpires2991@gmail.com>
2023-10-07 19:41:11 -0300

    Adicionando CaixaDeBonus.Java

---
+++ src/br/gov/cesarschool/poo/bonusvendas/dao/CaixaDeBonusDAO.java
@@ -0,0 +1,18 @@
+package br.gov.cesarschool.poo.bonusvendas.dao;
+
+import java.util.List;
+import br.gov.cesarschool.poo.bonusvendas.entidade.CaixaDeBonus;
+import br.gov.cesarschool.poo.bonusvendas.PersistenciaObjetos.br.edu.cesarschool.next.oo.persistenciaobjetos.CadastroObjetos;
+
+public class CaixaDeBonusDAO{
+        // Usando a Cadastro.Objetos.class
+        private CadastroObjetos cadastroObjetos;
+
+        public CaixaDeBonusDAO(){
+            this.CadastroObjetos = new CadastroObjetos(CaixaDeBonus.class);
+        }
+
+        public void salvarCaixaDeBonus(CaixaDeBonus caixaDeBonus, String chave){
+            cadastroObjetos.incluir(caixaDeBonus, chave);   
+        }
+}
