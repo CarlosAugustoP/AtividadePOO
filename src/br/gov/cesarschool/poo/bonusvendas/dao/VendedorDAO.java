fca8814c6fd64bf044700e6d8b439c60314adf6a
Pedro Andriotti <phab@cesar.school>
2023-10-07 19:28:50 -0300

    CpfValidator ok

---
+++ src/br/gov/cesarschool/poo/bonusvendas/dao/VendedorDAO.java
@@ -0,0 +1,15 @@
+package br.gov.cesarschool.poo.bonusvendas.dao;
+
+import br.gov.cesarschool.poo.bonusvendas.entidade.Vendedor;
+
+public class VendedorDAO {
+
+  incluirVendedor(Vendedor vendedor) {}
+
+  alterarVendedor(Vendedor vendedor) {}
+
+  excluirVendedor(Vendedor vendedor) {}
+
+  void buscarVendedorPorId(int id) {
+  }
+}
