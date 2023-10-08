2ae8a432c96958f75ada29ed4ead656ef609568f
kauan <novellokauan@gmail.com>
2023-10-07 22:51:04 -0300

    ResultadoInclusaoVendedor.java

--- src/br/gov/cesarschool/poo/bonusvendas/negocio/geral/StringUtil.Java
+++ src/br/gov/cesarschool/poo/bonusvendas/negocio/geral/StringUtil.Java
@@ -3,13 +3,13 @@ package br.gov.cesarschool.poo.bonusvendas.negocio.geral;
 public class StringUtil{
 
     private StringUtil(){
-        
+
     }
     public static boolean ehNuloOuBranco(String str){
         if(str == null){
             return true;
         }
-
+        // Remove espaços em branco à direita e à esquerda e verifica se a string está vazia
         str = str.trim();
         return str.isEmpty();
     }
