11a8797c28159f4061918c1c2834f1a7e9a523db
dan-albuquerque <daniloalbuquerquemelo@gmail.com>
2023-11-29 18:30:30 -0300

    parte extra feita

--- src/br/gov/cesarschool/poo/bonusvendas/daov2/DAOGenericoTp.java
+++ src/br/gov/cesarschool/poo/bonusvendas/daov2/DAOGenericoTp.java
@@ -8,60 +8,63 @@ import br.gov.cesarschool.poo.bonusvendas.excecoes.ExcecaoObjetoJaExistente;
 import br.gov.cesarschool.poo.bonusvendas.excecoes.ExcecaoObjetoNaoExistente;
 
 public class DAOGenericoTp<T extends Serializable> {
-    private String nomeEntidade;
-    private CadastroObjetos cadastro;
+	private String nomeEntidade;
+	private CadastroObjetos cadastro;
 
-    public DAOGenericoTp(Class<T> tipo, String nomeEntidade) {
-        cadastro = new CadastroObjetos(tipo);
-        this.nomeEntidade = nomeEntidade;
-    }
+	public void incluir(T reg) throws ExcecaoObjetoJaExistente {
+		String idUnico = ((Registro) reg).getIdUnico();
+		try {
+			buscar(idUnico);
+			throw new ExcecaoObjetoJaExistente(nomeEntidade + " ja existente");
+		} catch (ExcecaoObjetoNaoExistente e) {
+			cadastro.incluir(reg, idUnico);
+		}
+	}
 
-    public void incluir(T entidade) throws ExcecaoObjetoJaExistente {
-        String idUnico = ((Registro) entidade).getIdUnico();
-        try {
-            buscar(idUnico);
-            throw new ExcecaoObjetoJaExistente(nomeEntidade + " já existente");
-        } catch (ExcecaoObjetoNaoExistente e) {
-            cadastro.incluir(entidade, idUnico);
-        }
-    }
+	public void alterar(T reg) throws ExcecaoObjetoNaoExistente {
+		String idUnico = ((Registro) reg).getIdUnico();
+		try {
+			buscar(idUnico);
+			cadastro.alterar(reg, idUnico);
+		} catch (ExcecaoObjetoNaoExistente e) {
+			throw new ExcecaoObjetoNaoExistente(nomeEntidade + " nao existente");
+		}
+	}
 
-    public void alterar(T entidade) throws ExcecaoObjetoNaoExistente {
-        String idUnico = ((Registro) entidade).getIdUnico();
-        try {
-            buscar(idUnico);
-            cadastro.alterar(entidade, idUnico);
-        } catch (ExcecaoObjetoNaoExistente e) {
-            throw new ExcecaoObjetoNaoExistente(nomeEntidade + " não existente");
-        }
-    }
+	public boolean excluir(String id) throws ExcecaoObjetoNaoExistente {
+		try {
+			T regBusca = buscar(id);
+			if (regBusca == null) {
+				return false;
+			} else {
+				cadastro.excluir(id);
+				return true;
+			}
+		} catch (ExcecaoObjetoNaoExistente e) {
+			return false;
+		}
+	}
 
-    public boolean excluir(String id) throws ExcecaoObjetoNaoExistente {
-        try {
-            T entidadeBusca = buscar(id);
-            if (entidadeBusca == null) {
-                return false;
-            } else {
-                cadastro.excluir(id);
-                return true;
-            }
-        } catch (ExcecaoObjetoNaoExistente e) {
-            return false;
-        }
-    }
+	@SuppressWarnings("unchecked")
+	public T buscar(String id) throws ExcecaoObjetoNaoExistente {
+		if (cadastro.buscar(id) == null) {
+			throw new ExcecaoObjetoNaoExistente(nomeEntidade + " nao existente");
+		} else {
+			return (T) cadastro.buscar(id);
+		}
+	}
 
-    public T buscar(String id) throws ExcecaoObjetoNaoExistente {
-        if (cadastro.buscar(id) == null) {
-            throw new ExcecaoObjetoNaoExistente(nomeEntidade + " não existente");
-        } else {
-            return (T) cadastro.buscar(id);
-        }
-    }
 
-    public T[] buscarTodos() {
-        Serializable[] entidades = cadastro.buscarTodos(cadastro.getTipo());
-        T[] entidadesRet = (T[]) java.lang.reflect.Array.newInstance(cadastro.getTipo(), entidades.length);
-        System.arraycopy(entidades, 0, entidadesRet, 0, entidades.length);
-        return entidadesRet;
-    }
+	@SuppressWarnings("unchecked")
+	public T[] buscarTodos() {
+		Serializable[] regs = cadastro.buscarTodos();
+		T[] regsRet = (T[]) new Object[regs.length];
+
+		int i = 0;
+		while (i < regs.length) {
+			regsRet[i] = (T) regs[i];
+			i++;
+		}
+		return regsRet;
+	}
 }
